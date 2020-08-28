941d1c49df0b366489da3f6d98230330de28b054
tatar6607 <tatar66@gmail.com>
2020-08-28 23:28:13 +0200

    testbase update local ozkan to remote ozkan

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -8,11 +8,24 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
+import pages.DemoqaPage;
+
 import java.io.IOException;
 public class TestBase {
     public ExtentReports extentReports;
     public ExtentTest extentTest;
     public ExtentHtmlReporter extentHtmlReporter;
+
+    protected DemoqaPage demoqaPage;
+
+    @BeforeMethod
+    public void setup() {
+        Driver.getDriver().get(ConfigReader.getProperty("demoqa_url"));
+        demoqaPage = new DemoqaPage();
+        Driver.getDriver().manage().window().maximize();
+    }
+
+
     @BeforeTest
     public void setUpTest(){
         extentReports = new ExtentReports();// ExtentReports objesi üretelim
