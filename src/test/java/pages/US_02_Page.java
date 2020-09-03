893d9a4d5985356ad637268ed30e9acd1c8b75e7
tatar6607 <tatar66@gmail.com>
2020-09-04 00:19:49 +0200

    local ozkan to remote 4. gun

--- src/test/java/pages/US_02_Page.java
+++ src/test/java/pages/US_02_Page.java
@@ -1,4 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_02_Page {
+    public US_02_Page() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id="item-1")
+    public WebElement checkboxMenuLink;
+
+    @FindBy(xpath = "//div[@class='main-header']")
+    public WebElement checkBoxHeader;
+
+
+
 }
