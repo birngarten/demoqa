7c7332d5955aef9068fab949de980e02a5938858
tatar6607 <tatar66@gmail.com>
2020-09-03 22:53:50 +0200

    us_02_page 4. gun

--- src/test/java/pages/US_02_Page.java
+++ src/test/java/pages/US_02_Page.java
@@ -1,4 +1,13 @@
 package pages;
 
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
 public class US_02_Page {
+
+    public US_02_Page() {
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
 }
