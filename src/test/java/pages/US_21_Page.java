fe73aaf8556ea422ae8b65053a43f877b7558bd6
Saltuk <saltukarkali@gmail.com>
2020-10-05 23:39:15 +0200

    Local Saltuk son commit 05102020

--- src/test/java/pages/US_21_Page.java
+++ src/test/java/pages/US_21_Page.java
@@ -13,5 +13,25 @@ public class US_21_Page {
     @FindBy(xpath = "(//div[@class='avatar mx-auto white'])[4]")
     public WebElement widgetsCard;
 
+    @FindBy(xpath = "//*[.='Tool Tips']")
+    public WebElement linkToolTips;
+
+    @FindBy(xpath = "//div[@class='main-header']")
+    public WebElement mainHeader;
+
+    @FindBy(xpath = "//*[.='Practice Tool Tips']")
+    public WebElement subHeader;
+
+    @FindBy(id = "toolTipButton")
+    public WebElement buttonHover;
+
+    @FindBy(id = "toolTipTextField")
+    public WebElement bosTextBox;
+
+    @FindBy(xpath = "//div[@class='tooltip-inner']")
+    public WebElement hoverGizliText;
+
+    @FindBy(id = "texToolTopContainer")
+    public WebElement doluText;
 
 }
