b7564e7d5645335e00900e082479f97dc2240c5c
birngarten <resitozyurt@gmail.com>
2020-10-27 16:33:32 +0100

    27.10.2020

--- src/test/java/pages/US_33_Page.java
+++ src/test/java/pages/US_33_Page.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -8,6 +10,11 @@ public class US_33_Page {
     public US_33_Page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(xpath = "//div[@class='card-up']")
+    public WebElement elementsCard;
+
+
+
 
 
 }
