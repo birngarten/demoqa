6f02230881bcc623da88c19652e73d8039009bc8
birngarten <resitozyurt@gmail.com>
2020-10-28 22:22:40 +0100

    28.10.2020

--- src/test/java/pages/US_33_Page.java
+++ src/test/java/pages/US_33_Page.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class US_33_Page {
 
     public US_33_Page(){
@@ -12,6 +14,21 @@ public class US_33_Page {
     }
     @FindBy(xpath = "//div[@class='card-up']")
     public WebElement elementsCard;
+
+    @FindBy(xpath = "//span[.='Broken Links - Images']")
+    public WebElement brokenLinksMenu;
+
+    @FindBy(xpath = "//p")
+    public List<WebElement> brokenLinkWebElementList;
+
+    @FindBy(xpath = "(//img[@src='/images/Toolsqa.jpg'])[2]")
+    public WebElement validImage;
+
+    @FindBy(xpath = "//img[@src='/images/Toolsqa_1.jpg']")
+    public WebElement brokenImage;
+
+    @FindBy(xpath = "(//a)[2]")
+    public WebElement validLink;
 
 
 
--- src/test/java/tests/US_33_Test.java
+++ src/test/java/tests/US_33_Test.java
@@ -1,10 +1,14 @@
 package tests;
 
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import pages.US_33_Page;
+import utilities.Driver;
+import utilities.ReusableMethods;
 import utilities.TestBase;
 
 public class US_33_Test extends TestBase {
@@ -15,44 +19,56 @@ public class US_33_Test extends TestBase {
     public void brokenSetup(){
         us33Page.elementsCard.click();
         actions.sendKeys(Keys.PAGE_DOWN).perform();
-
+        wait.until(ExpectedConditions.elementToBeClickable(us33Page.brokenLinksMenu)).click();
 
     }
 
     @Test
     public void TC195(){
 // Broken Links - Images linkinin tıklanabildigini assert ediniz
-
+        Assert.assertTrue(us33Page.brokenLinksMenu.isEnabled());
 
     }
 
     @Test
     public void TC196(){
 // Sayfada sırasıyla Valid image, Broken image, Valid Link ve Broken Link elemanlarinin oldugunu assert ediniz
+        String brokenLinkElemanlari[]= {"Valid image", "Broken image", "Valid Link", "Broken Link"};
 
-
-
+        int i =0;
+        for (WebElement w: us33Page.brokenLinkWebElementList){
+            System.out.println(w.getText());
+            Assert.assertEquals(w.getText(),brokenLinkElemanlari[i++]);
+        }
     }
 
     @Test
     public void TC197(){
 // Valid image ın gorutulendigini assert ediniz
-
+        Assert.assertTrue(us33Page.validImage.isDisplayed());
 
     }
 
     @Test
     public void TC198(){
 // Broken image in goruntulenmedigini assert ediniz
-
+        Assert.assertTrue(us33Page.brokenImage.getAttribute("naturalWidth").equals("0"));
 
     }
 
     @Test
     public void TC199(){
 // Valid linke tıklandiginda demoqa anasayfaya ulasildini assert ediniz
+        String validLinkUrl = Driver.getDriver().getCurrentUrl();
+        System.out.println(validLinkUrl);
 
+        ReusableMethods.waitFor(2);
+        actions.click(us33Page.validLink).build().perform();
 
+        String demoqaUrl = Driver.getDriver().getCurrentUrl();
+        System.out.println(demoqaUrl);
+
+        Assert.assertEquals(demoqaUrl,"https://demoqa.com/");
 
     }
 
