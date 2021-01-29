4324097a1de1d40708cf0d59d7d3abf647aee3e0
birngarten <resitozyurt@gmail.com>
2021-01-29 18:55:47 +0100

    29.01.2021

--- src/test/java/pojos/DemoqaGetPojo.java
+++ src/test/java/pojos/DemoqaGetPojo.java
@@ -1,63 +1,63 @@
 package pojos;
 
 import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.codehaus.jackson.annotate.JsonProperty;
+
 
 import java.util.List;
 
 public class DemoqaGetPojo {
 
-
-    @JsonProperty("userId")
-    private String userId;
-    @JsonProperty("username")
-    private String username;
-    @JsonProperty("books")
-    private List<Book> books = null;
-
-    public DemoqaGetPojo() {
-    }
-
-    public DemoqaGetPojo(String userId, String username, List<Book> books) {
-        super();
-        this.userId = userId;
-        this.username = username;
-        this.books = books;
-    }
-
-    @JsonProperty("userId")
-    public String getUserId() {
-        return userId;
-    }
-
-    @JsonProperty("userId")
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    @JsonProperty("username")
-    public String getUsername() {
-        return username;
-    }
-
-    @JsonProperty("username")
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @JsonProperty("books")
-    public List<Book> getBooks() {
-        return books;
-    }
-
-    @JsonProperty("books")
-    public void setBooks(List<Book> books) {
-        this.books = books;
-    }
-
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this).append("userId", userId).append("username", username).append("books", books).toString();
-    }
+//
+//    @JsonProperty("userId")
+//    private String userId;
+//    @JsonProperty("username")
+//    private String username;
+//    @JsonProperty("books")
+//    private List<Book> books = null;
+//
+//    public DemoqaGetPojo() {
+//    }
+//
+//    public DemoqaGetPojo(String userId, String username, List<Book> books) {
+//        super();
+//        this.userId = userId;
+//        this.username = username;
+//        this.books = books;
+//    }
+//
+//    @JsonProperty("userId")
+//    public String getUserId() {
+//        return userId;
+//    }
+//
+//    @JsonProperty("userId")
+//    public void setUserId(String userId) {
+//        this.userId = userId;
+//    }
+//
+//    @JsonProperty("username")
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    @JsonProperty("username")
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    @JsonProperty("books")
+//    public List<Book> getBooks() {
+//        return books;
+//    }
+//
+//    @JsonProperty("books")
+//    public void setBooks(List<Book> books) {
+//        this.books = books;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return new ToStringBuilder(this).append("userId", userId).append("username", username).append("books", books).toString();
+//    }
 
 }
